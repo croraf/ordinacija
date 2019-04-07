2a544313d7c6bf6f417952b17ea54b7ca4b6cf20
croraf <vrafaeli@msn.com>
2019-04-07 16:08:05 +0200

    Remove dist. Minor change in contact

--- frontend/src/routes/Home/Contact/Contact.js
+++ frontend/src/routes/Home/Contact/Contact.js
@@ -14,14 +14,24 @@ class Contact extends React.Component {
                 display: 'flex',
                 flexDirection: 'column',
             }}>
+                <div style={{fontSize: '1.4rem', marginBottom: '1rem', fontWeight: 'bold'}}>Radno vrijeme</div>
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'space-between',
+                    marginBottom: '2.5rem'
+                }}>
+                    <div style={{marginBottom: '1rem'}}>Pon - Pet:  08:00 - 20:00h</div>
+                    <div style={{}}>Sub:  08:00 - 14:00h</div>
+                </div>
+
                 <div style={{fontSize: '1.4rem', marginBottom: '1rem', fontWeight: 'bold'}}>Kontaktirajte nas</div>
                 <div style={{
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'space-between',
-                    marginBottom: '3rem'
+                    marginBottom: '2.5rem'
                 }}>
-                    
                     <div style={{marginBottom: '1rem'}}>Telefon: +385 91 111222</div>
                     <div style={{}}>Mail: ordinacija@gmail.com</div>
                 </div>
@@ -31,7 +41,7 @@ class Contact extends React.Component {
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'space-between',
-                    marginBottom: '3rem'
+                    marginBottom: '2.5rem'
                 }}>
                     <div style={{marginBottom: '1rem'}}>Adresa: Zagrebačka 123, 10000 Zagreb</div>
                     <img src={mapUrl} width='500px' height='500px' style={{}} />
