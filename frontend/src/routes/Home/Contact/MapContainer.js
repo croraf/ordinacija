5d53ca677f4ab1308897cb2bf77f616f3c2c8830
croraf <vrafaeli@msn.com>
2019-04-07 21:20:38 +0200

    Reduce map component

--- frontend/src/routes/Home/Contact/MapContainer.js
+++ frontend/src/routes/Home/Contact/MapContainer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker';
 
@@ -9,55 +8,23 @@ class MapContainer extends React.Component {
     state = {
         center: [45.815, 15.9819],
         zoom: 13,
-        twoFingerDrag: false,
-        animate: true,
-        animating: false,
-        zoomSnap: true,
         minZoom: 1,
         maxZoom: 18,
-    };
-
-    zoomIn = () => {
-        this.setState({
-            zoom: Math.min(this.state.zoom + 1, 18)
-        });
-    }
-
-    zoomOut = () => {
-        this.setState({
-            zoom: Math.max(this.state.zoom - 1, 1)
-        });
-    }
-
-    handleBoundsChange = ({ center, zoom, bounds, initial }) => {
-        if (initial) {
-            console.log('Got initial bounds: ', bounds);
-        }
-        this.setState({ center, zoom });
     }
 
     render() {
 
-        const { center, zoom, animate, metaWheelZoom, twoFingerDrag, zoomSnap, 
-            minZoom, maxZoom } = this.state;
+        const { center, zoom, minZoom, maxZoom } = this.state;
 
         return (
             <div style={{width: '500px', border: '1px solid black'}}>
                 <Map 
-                    limitBounds='edge'
                     center={center}
                     zoom={zoom}
-                    onBoundsChanged={this.handleBoundsChange}
-                    animate={animate}
-                    twoFingerDrag={twoFingerDrag}
-                    zoomSnap={zoomSnap}
-                    mouseEvents={true}
-                    touchEvents={true}
                     minZoom={minZoom}
                     maxZoom={maxZoom}
                     defaultWidth={500}
                     height={500}
-                    style={{border: '1px solid black'}}
                 >
                     <Marker anchor={[45.815, 15.9819]} payload={1} onClick={({ event, anchor, payload }) => {}} />
                 </Map>
