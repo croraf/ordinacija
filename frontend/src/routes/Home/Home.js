67ecfabb1991bdcb5041a4f1e5074416debae7fe
croraf <vrafaeli@msn.com>
2019-04-10 21:44:27 +0200

    Changed title styling. Add title image

--- frontend/src/routes/Home/Home.js
+++ frontend/src/routes/Home/Home.js
@@ -40,49 +40,52 @@ class Home extends React.Component {
     render() {
 
         return (
-            <div style={{
-                /* position: 'relative',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'center', */
-                //backgroundColor: 'whitesmoke'
-                display: 'flex',
-                alignItems: 'center',
-                width: '95%',
-                maxWidth: '1000px',
-                margin: 'auto',
-                marginTop: '1rem',
-                flexDirection: 'column',
-            }}>
-                {/* <div style={{
-                    minWidth: '200px'
-                }}><img src='https://picsum.photos/200/600' /></div> */}
-
-                {/* <RecentOrderNotificationContainer /> */}
-
-                {/* <MediaQuery query='(max-device-width: 1700px)'>
-                    <div style={containerStyleHd}>
-                        <TitleRowContainer />
-                        <HomeRouter />
-                    </div>
-                </MediaQuery>
-
-                <MediaQuery query='(min-device-width: 1701px)'>
-                    <div style={containerStyleFullHd}>
-                        <TitleRowContainer />
-                        <HomeRouter />
-                    </div>
-                </MediaQuery> */}
-                
-                {/* <div style={{
-                    minWidth: '200px'
-                }}><img src='https://picsum.photos/300/300' /></div> */}
-
+            <>
                 <TitleRowContainer />
-                <HomeRouter />
+                <div style={{
+                    /* position: 'relative',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'center', */
+                    //backgroundColor: 'whitesmoke'
+                    display: 'flex',
+                    alignItems: 'center',
+                    width: '95%',
+                    maxWidth: '1000px',
+                    margin: 'auto',
+                    marginTop: '1rem',
+                    flexDirection: 'column',
+                }}>
+                    {/* <div style={{
+                        minWidth: '200px'
+                    }}><img src='https://picsum.photos/200/600' /></div> */}
+
+                    {/* <RecentOrderNotificationContainer /> */}
+
+                    {/* <MediaQuery query='(max-device-width: 1700px)'>
+                        <div style={containerStyleHd}>
+                            <TitleRowContainer />
+                            <HomeRouter />
+                        </div>
+                    </MediaQuery>
+
+                    <MediaQuery query='(min-device-width: 1701px)'>
+                        <div style={containerStyleFullHd}>
+                            <TitleRowContainer />
+                            <HomeRouter />
+                        </div>
+                    </MediaQuery> */}
+                    
+                    {/* <div style={{
+                        minWidth: '200px'
+                    }}><img src='https://picsum.photos/300/300' /></div> */}
+
+                    
+                    <HomeRouter />
+                    {/* <CartModalContainer /> */}
+                </div>
                 <div style={{width: '100%', backgroundColor: 'black', color: 'white'}}>Footer</div>
-                {/* <CartModalContainer /> */}
-            </div>
+            </>
         );
     }
 }
--- frontend/src/routes/Home/TitleRow/TitleRow.js
+++ frontend/src/routes/Home/TitleRow/TitleRow.js
@@ -5,73 +5,86 @@ import { CartButtonContainer } from './CartButton/CartButtonContainer'; */
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 
+import woman from './BeautifulWoman.jpg';
+
 class TitleRow extends React.Component {
     render() {
         const {servicesClickHadnler, contactClickHandler, logoClickedHandler, currentRoute} = this.props;
 
         return (
-            <Grid container style={{
-                width: '100%',
-                margin: '15px 0px 1rem 0px',
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-            }}>
-                <Grid item xs={12} md={6}
-                    /* onClick={logoClickedHandler} */
-                    style={{
-                        fontSize: '2.3rem',
-                        //cursor: 'pointer',
-                        fontWeight: 'bold',
-                        color: 'black',
-                        marginBottom: '1rem'
-                    }}
-                >
-                    Ordinacija Zekić
-                </Grid>
+            <>
+                <Grid container style={{
+                    position: 'fixed',
+                    top: '0rem',
+                    backgroundColor: 'whitesmoke',
+                    width: '100%',
+                    margin: 'auto',
+                    padding: '1.5rem 1.5rem 0rem 1.5rem',
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    borderBottom: '1px solid black',
+                    /* opacity: '0.7' */
+                }}>
+                    <Grid item xs={12} md={6}
+                        /* onClick={logoClickedHandler} */
+                        style={{
+                            fontSize: '2.3rem',
+                            //cursor: 'pointer',
+                            fontWeight: 'bold',
+                            color: 'black',
+                            marginBottom: '1rem'
+                        }}
+                    >
+                        Ordinacija Zekić
+                    </Grid>
 
-                
-                <Grid item xs={12} md={6}
-                    style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginBottom: '1rem'}}
-                >
-                    <Grid container>
-                        <Grid item xs={false} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                           
-                        </Grid>
-                        <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                            <Link 
-                                onClick={servicesClickHadnler}
-                                style={{fontSize: '1.2rem', cursor: currentRoute === '/' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/' && 'bold',}}
-                                underline='none'
-                            >
-                                O nama
-                            </Link>
-                        </Grid>
-                        <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                            <Link 
-                                onClick={servicesClickHadnler}
-                                style={{fontSize: '1.2rem', cursor: currentRoute === '/' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/' && 'bold',}}
-                                underline='none'
-                            >
-                                Usluge
-                            </Link>
-                        </Grid>
-                        <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
-                            <Link 
-                                onClick={contactClickHandler}
-                                style={{fontSize: '1.2rem', cursor: currentRoute === '/contact' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/contact' && 'bold',}}
-                                underline='none'
-                            >
-                                Kontakt
-                            </Link>
+                    
+                    <Grid item xs={12} md={6}
+                        style={{display: 'flex', justifyContent: 'flex-end', alignItems: 'center', marginBottom: '1rem'}}
+                    >
+                        <Grid container>
+                            <Grid item xs={false} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'right'}}>
+                            
+                            </Grid>
+                            <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'right'}}>
+                                <Link 
+                                    onClick={servicesClickHadnler}
+                                    style={{fontSize: '1.2rem', cursor: currentRoute === '/' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/' && 'bold',}}
+                                    underline='none'
+                                >
+                                    O nama
+                                </Link>
+                            </Grid>
+                            <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'right'}}>
+                                <Link 
+                                    onClick={servicesClickHadnler}
+                                    style={{fontSize: '1.2rem', cursor: currentRoute === '/' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/' && 'bold',}}
+                                    underline='none'
+                                >
+                                    Usluge
+                                </Link>
+                            </Grid>
+                            <Grid item xs={4} md={3} style={{display: 'flex', flexDirection: 'row', justifyContent: 'right'}}>
+                                <Link 
+                                    onClick={contactClickHandler}
+                                    style={{fontSize: '1.2rem', cursor: currentRoute === '/contact' ? 'default' : 'pointer', color: 'black', fontWeight: currentRoute === '/contact' && 'bold',}}
+                                    underline='none'
+                                >
+                                    Kontakt
+                                </Link>
+                            </Grid>
                         </Grid>
                     </Grid>
-                </Grid>
 
-                {/*THIS IS A HACK TO ADD EXTRA SPACE BELOW TITLE WHEN SIZE IS LARGER THAN md*/}
-                <Grid item xs={6} md={12} style={{height: '1rem'}} />
-                <Grid item xs={6} md={12} style={{height: '1rem'}} />
-            </Grid>
+                    {/*THIS IS A HACK TO ADD EXTRA SPACE BELOW TITLE WHEN SIZE IS LARGER THAN md*/}
+                    <Grid item xs={6} md={12} style={{height: '0.5rem'}} />
+                    <Grid item xs={6} md={12} style={{height: '0.5rem'}} />
+                </Grid>
+                <div style={{width: '100%', height: '700px', overflow: 'hidden', textAlign: 'center', marginTop: '6rem'}}>
+                    <img src={woman} style={{width: '100%'}}/> 
+                </div>
+            </>
         );
     }
 }
